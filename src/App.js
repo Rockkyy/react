f84470d12a2e42cd9e26364e7e40f9325f200cab
rocky <serg9191@mail.ru>
2022-03-07 18:49:43 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,189 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    return (
+        <>
+
+   
+            <body>
+            <header>
+                <div class="container">
+                    <div class="header-content">
+                        <a href="">ГЛАВНАЯ</a>
+                        <a href="">ОБ АВТОРЕ</a>
+                        <a href="">РАБОТЫ</a>
+                        <a href="">ПРОЦЕСС</a>
+                        <a href="">КОНТАКТЫ</a>
+                    </div>
+                </div>
+            </header>
+
+
+            <section class="screen">
+                <div class="container">
+                    <div class="screen-content">
+                        <div class="left">
+                            <img src="img/screen.png" alt="" />
+                </div>
+                            <div class="right">
+                                <h1>Дизайн и верстка</h1>
+                                <p>
+                                    Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                                    стандартной "рыбой" для текстов на латинице с начала XVI века.
+                    </p>
+                                <button>НАПИСАТЬ МНЕ</button>
+                            </div>
+                        </div>
+                    </div>
+    </section>
+
+
+                <section class="about">
+                    <div class="container">
+                        <div class="about-content">
+                            <h1>Обо мне</h1>
+                            <p>
+                                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                                стандартной "рыбой" для текстов на латинице с начала XVI века.
+                </p>
+                        </div>
+                    </div>
+                </section>
+
+
+                <section class="features">
+                    <div class="container">
+                        <div class="features-content">
+                            <div class="feature">
+                                <img src="img/features/1.png" alt="" />
+                                    <div class="text">
+                                        <h3>40+</h3>
+                                        <span>проектов</span>
+                                    </div>
+                </div>
+                                <div class="feature">
+                                    <img src="img/features/2.png" alt="" />
+                                        <div class="text">
+                                            <h3>40+</h3>
+                                            <span>проектов</span>
+                                        </div>
+                </div>
+                                    <div class="feature">
+                                        <img src="img/features/3.png" alt="" />
+                                            <div class="text">
+                                                <h3>40+</h3>
+                                                <span>проектов</span>
+                                            </div>
+                </div>
+                                        <div class="feature">
+                                            <img src="img/features/4.png" alt="" />
+                                                <div class="text">
+                                                    <h3>40+</h3>
+                                                    <span>проектов</span>
+                                                </div>
+                </div>
+                                            <div class="feature">
+                                                <img src="img/features/5.png" alt="" />
+                                                    <div class="text">
+                                                        <h3>40+</h3>
+                                                        <span>проектов</span>
+                                                    </div>
+                </div>
+                                                <div class="feature">
+                                                    <img src="img/features/6.png" alt="" />
+                                                        <div class="text">
+                                                            <h3>40+</h3>
+                                                            <span>проектов</span>
+                                                        </div>
+                </div>
+                                                </div>
+                                            </div>
+    </section>
+
+                                        <section class="hero">
+                                            <div class="container">
+                                                <div class="hero-content">
+                                                    <div class="left">
+                                                        <h2>Верстальщик</h2>
+                                                        <h1>Тиль Швайгер</h1>
+                                                        <p>
+                                                            Разрабатываю сайты для малого и среднего бизнеса. У меня можно заказать дизайн и верстку
+                                                            небольшого сайта или интернет-магазина
+                    </p>
+                                                        <button>НАПИСАТЬ МНЕ</button>
+                                                    </div>
+                                                    <div class="right">
+                                                        <img src="img/hero.png" alt="" />
+                </div>
+                                                    </div>
+                                                </div>
+    </section>
+
+                                            <section class="video">
+                                                <div class="container">
+                                                    <div class="video-content">
+                                                        <h1>Как я работаю</h1>
+                                                        <p>
+                                                            Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                                                            стандартной "рыбой" для текстов на латинице с начала XVI века.
+                </p>
+                                                        <iframe src="https://www.youtube.com/embed/9Wi4UA549A8" frameborder="0" 
+                                                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                                            allowfullscreen></iframe>
+                                                    </div>
+                                                </div>
+                                            </section>
+
+                                            <section class="feedback">
+                                                <div class="container">
+                                                    <div class="feedback-content">
+                                                        <h1>Хотите веб-сайт?</h1>
+                                                        <p>
+                                                            Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                                                            стандартной "рыбой" для текстов на латинице с начала XVI века.
+                </p>
+                                                        <form action="">
+                                                            <input type="text" placeholder="Ваше имя" />
+                                                                <input type="email" placeholder="Ваш e-mail" />
+                                                                    <textarea placeholder="Ваше сообщение"></textarea>
+                                                                    <button>ОТПРАВИТЬ</button>
+                </form>
+            </div>
+        </div>
+    </section>
+
+
+</body>
+
+
+
+
+
+        </>
+
+    );
+
+} 
 
 export default App;
+
+/*
+export function Menu() {
+
+    return (
+
+        <div>
+
+            <a href="#"> Link to google </a>
+
+            <a href="#"> Link to yandex </a>
+
+        </div>
+
+    )
+
+}
+*/
